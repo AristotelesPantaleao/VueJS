5000959f5bcfac6ec909bb2d54b5d7b1464b8814
AristotelesPantaleao <62292967+AristotelesPantaleao@users.noreply.github.com>
2021-03-10 14:41:04 -0300

    Create index.js

---
+++ most-used-words/src/backend/index.js
@@ -0,0 +1,21 @@
+const { ipcMain } = require('electron')
+
+const pathsToRows = require('./pathsToRow')
+
+
+ipcMain.on('process-subtitles', (event, paths) => {
+
+    pathsToRows(paths).then(rows => console.log(rows))
+        .then(() => {
+            event.reply('process-subtitles', [
+                { name: 'i', amount: 1234 },
+                { name: 'you', amount: 900 },
+                { name: 'he', amount: 853 },
+                { name: 'she', amount: 853 },
+                { name: 'our', amount: 133 },
+                { name: 'house', amount: 23 },
+            ])
+
+        })
+
+})
